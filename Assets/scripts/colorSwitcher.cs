306225bebc9fa2fb1d42ae72df438491ec105e0e
unknown <manu.amg95@gmail.com>
2018-10-29 11:31:09 -0600

    ejemplo de colision

---
+++ Assets/scripts/colorSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorSwitcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+        Destroy(gameObject, 2.0f);
+    }
+}
